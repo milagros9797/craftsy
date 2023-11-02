5bbf1707389378f2df15d28b1ff77d916c25e63c
Milagros <miilyailen97@gmail.com>
2023-11-02 10:12:35 -0300

    estilos en form.css y register.css

--- app.js
+++ app.js
@@ -9,7 +9,12 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "views", "home.html"))
 );
-
+app.get("/login", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "login.html"))
+);
+app.get("/register", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "register.html"))
+);
 app.listen(PORT, () =>
   console.log(`Server running in http://localhost:${PORT}`)
 );
